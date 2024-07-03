0745e2e98380555c8f88f33e2f2eca236cb63e39
seahi <me@seahi.me>
2024-07-03 15:22:02 +0800

    refactor: renamed several fields in response

--- model/response.go
+++ model/response.go
@@ -15,11 +15,9 @@ type TestResponse struct {
 }
 
 type Page struct {
-	PageSize     int    `json:"pageSize,omitempty"`
-	PageIndex    int    `json:"pageIndex,omitempty"`
-	ContentCount int64  `json:"total,omitempty"`
-	PageCount    int64  `json:"pageCount,omitempty"`
-	Next         string `json:"next,omitempty"`
-	Pre          string `json:"pre,omitempty"`
-	Order        string `json:"orderBy,omitempty"`
+	PageSize   int    `json:"pageSize,omitempty"`
+	PageIndex  int    `json:"pageIndex,omitempty"`
+	ItemsCount int64  `json:"itemsCount,omitempty"`
+	PageCount  int64  `json:"pageCount,omitempty"`
+	Order      string `json:"orderBy,omitempty"`
 }
